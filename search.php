9399ac602bde50bf90b4f80e3d175fb6bd912161
mmorgan86 <michael1986morgan@gmail.com>
2019-01-12 17:12:23 -0500

    added customer search query to interact with database

--- search.php
+++ search.php
@@ -14,15 +14,30 @@
 
 			<?php
 
-            $query = "SELECT * FROM posts";
-            $selctAllPostQuery = mysqli_query($connection, $query);
+        if(isset($_POST['submit'])){
+          $search = $_POST['search'];
 
-            while($row = mysqli_fetch_assoc($selctAllPostQuery)){
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = $row['post_content'];
+          $query = "SELECT * FROM posts 
+                    WHERE post_tags LIKE '%$search%' 
+                    OR post_title LIKE '%$search%'";
+          $searchQuery = mysqli_query($connection, $query);
+
+          if(!$searchQuery) {
+            die("SEARCH QUERY FAILED ". mysqli_error($connection));
+          }
+
+          $count = mysqli_num_rows($searchQuery);
+
+          if($count === 0){
+            echo "<h1>No Result</h1>";
+          } else {
+            
+          while($row = mysqli_fetch_assoc($searchQuery)){
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
 
       ?>
 
@@ -54,7 +69,12 @@
 			<hr>
 
 
-			<?php } ?>
+      <?php 
+            }
+          }
+        }
+      ?>
+          
 		</div>
 
 		<!-- Blog Sidebar Widgets Column -->
