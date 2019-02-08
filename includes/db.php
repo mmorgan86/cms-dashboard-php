3957428b8a0f178bf043deb11b0847403841bbd1
mmorgan86 <michael1986morgan@gmail.com>
2019-02-07 23:01:46 -0500

    updated db connection to connect heroku to jawsDB MySQL database

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-  $db['db_host'] = "localhost";
-  $db['db_username'] = "root";
-  $db['db_password'] = "";
-  $db['db_name'] = "cms";
+  $db['db_host'] = "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+  $db['db_username'] = "ivancnf75wfzdbcs";
+  $db['db_password'] = "in8l5tj2l4dqhmh2";
+  $db['db_name'] = "ju6j8cr35erg1fts";
 
   foreach($db as $key => $value){
     define(strtoupper($key), $value);
