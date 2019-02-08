c778932abb56e24b5e6855f051aa2e5b3f6b8586
mmorgan86 <michael1986morgan@gmail.com>
2019-02-07 23:40:19 -0500

    updated connection

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,32 @@
 <?php
 
-  $db['db_host'] = "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-  $db['db_username'] = "ivancnf75wfzdbcs";
-  $db['db_password'] = "in8l5tj2l4dqhmh2";
-  $db['db_name'] = "ju6j8cr35erg1fts";
+$url = getenv('mysql://ivancnf75wfzdbcs:in8l5tj2l4dqhmh2@lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ju6j8cr35erg1fts');
 
-  foreach($db as $key => $value){
-    define(strtoupper($key), $value);
-  }
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
-  $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection was successfully established!";
+
+
+
+////// old connection to db /////////
+  // $db['db_host'] = "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+  // $db['db_username'] = "ivancnf75wfzdbcs";
+  // $db['db_password'] = "in8l5tj2l4dqhmh2";
+  // $db['db_name'] = "ju6j8cr35erg1fts";
+
+  // foreach($db as $key => $value){
+  //   define(strtoupper($key), $value);
+  // }
+
+  // $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 ?>
