4da568d603c0c371ca951064a18212feea8280d4
mmorgan86 <michael1986morgan@gmail.com>
2019-02-07 23:44:58 -0500

    fixed connection error

--- includes/db.php
+++ includes/db.php
@@ -2,17 +2,17 @@
 
 $url = getenv('mysql://ivancnf75wfzdbcs:in8l5tj2l4dqhmh2@lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ju6j8cr35erg1fts');
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+$hostname = 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'ivancnf75wfzdbcs';
+$password = 'in8l5tj2l4dqhmh2';
+$database = 'ju6j8cr35erg1fts';
 
 // Create connection
-$conn = new mysqli($hostname, $username, $password, $database);
+$connection = new mysqli($hostname, $username, $password, $database);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
 }
 echo "Connection was successfully established!";
 
