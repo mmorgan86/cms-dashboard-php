a65243aefb69e571beadb3b856a1fcf990c118b6
mmorgan86 <michael1986morgan@gmail.com>
2019-02-08 20:34:33 -0500

    fixed pagenation to work on page load

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
 		<?php
 			for($i = 1; $i <= $count; $i++) {
 
-				if($i == $page) {
+				if($i == $page || $i == 1) {
 					echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
 				} else {
 				echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
