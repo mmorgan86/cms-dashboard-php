7f91e2dad59da26c219ec1a4f3e47f481d234f37
mmorgan86 <michael1986morgan@gmail.com>
2019-02-08 20:20:47 -0500

    updated db to cleardb final

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,5 @@
 <?php
 
-// JAWSDB MYSQL CONNECTION
-
-// $url = getenv('mysql://ivancnf75wfzdbcs:in8l5tj2l4dqhmh2@lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ju6j8cr35erg1fts');
-
-
-// $hostname = 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-// $username = 'ivancnf75wfzdbcs';
-// $password = 'in8l5tj2l4dqhmh2';
-// $database = 'ju6j8cr35erg1fts';
-
 
 // CLEARDB CONNECTION
 $hostname = 'us-cdbr-iron-east-03.cleardb.net';
@@ -25,31 +15,4 @@ if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 } 
 
-// else {
-//   $hostname = 'localhost';
-//   $username = 'root';
-//   $password = '';
-//   $database = 'cms';
-
-// Create connection
-$connection = new mysqli($hostname, $username, $password, $database);
-
-
-echo "connected";
-
-// }
-
-
-
-////// old connection to db /////////
-  // $db['db_host'] = "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-  // $db['db_username'] = "ivancnf75wfzdbcs";
-  // $db['db_password'] = "in8l5tj2l4dqhmh2";
-  // $db['db_name'] = "ju6j8cr35erg1fts";
-
-  // foreach($db as $key => $value){
-  //   define(strtoupper($key), $value);
-  // }
-
-  // $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 ?>
