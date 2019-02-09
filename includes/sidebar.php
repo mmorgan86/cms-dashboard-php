4b081d0b33ef731e289e83c8a071ee814fe2a068
mmorgan86 <michael1986morgan@gmail.com>
2019-02-08 21:42:54 -0500

    sidebar: create logout feature if user is logged in

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -31,9 +31,39 @@
 					<input name="password" type="password" class="form-control border border-danger" placeholder="Enter Password" required="Please enter a valid username">
 					<span class="input-group-btn">
 						<button class="btn btn-success" name="login" type="submit">Login</button>
-						<a href="registration.php"><button class="btn btn-primary"> Register</button></a>
 					</span>
 					
+					<!-- Page Content -->
+<div class="container">
+
+<section id="login">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-6 col-xs-offset-3">
+				<div class="form-wrap">
+					<h4 class="text-center">Register</h4>
+					<form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+						<h6 class="text-center"><?php echo $message ?></h6>
+						<div class="form-group">
+							<label for="username" class="sr-only">username</label>
+							<input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+						</div>
+						<div class="form-group">
+							<label for="email" class="sr-only">Email</label>
+							<input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+						</div>
+						<div class="form-group">
+							<label for="password" class="sr-only">Password</label>
+							<input type="password" name="password" id="key" class="form-control" placeholder="Password">
+						</div>
+
+						<input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+					</form>
+
+				</div>
+			</div> <!-- /.col-xs-12 -->
+		</div> <!-- /.row -->
+	</div> <!-- /.container -->
 				</div>
 			</form> <!-- search form --->
 			<!-- /.input-group -->
