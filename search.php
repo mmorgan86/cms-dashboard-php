19036643d4864b273c6926214c6a2cb3482d28f7
mmorgan86 <michael1986morgan@gmail.com>
2019-02-08 22:41:04 -0500

    search.php:added mysqli injection protection

--- search.php
+++ search.php
@@ -15,7 +15,9 @@
 			<?php
 
         if(isset($_POST['submit'])){
-          $search = $_POST['search'];
+					$search = $_POST['search'];
+					
+					$search = mysqli_real_escape_string($connection, $_POST['submit']);
 
           $query = "SELECT * FROM posts 
                     WHERE post_tags LIKE '%$search%' 
